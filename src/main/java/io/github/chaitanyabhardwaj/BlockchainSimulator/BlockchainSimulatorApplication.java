48fb846ec0e79715ab986c1ea81c6132ae30ee9a
Chaitanya Bhardwaj <chaitanyabhardwaj89@gmail.com>
2024-02-25 20:18:05 +0530

    Initial commit

---
+++ src/main/java/io/github/chaitanyabhardwaj/BlockchainSimulator/BlockchainSimulatorApplication.java
@@ -0,0 +1,13 @@
+package io.github.chaitanyabhardwaj.BlockchainSimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockchainSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockchainSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/chaitanyabhardwaj/BlockchainSimulator/BlockchainSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.chaitanyabhardwaj.BlockchainSimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockchainSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
